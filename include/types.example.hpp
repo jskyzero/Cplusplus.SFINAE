97465c4d5046ec06ab98daf0ea7e33e1f5c271fb
jskyzero <jskyzero@outlook.com>
2019-04-15 13:37:14 +0800

    :ambulance: decicd to abandon treatment

--- include/types.example.hpp
+++ include/types.example.hpp
@@ -10,12 +10,13 @@ struct A {
 // this version fails on gcc (Ubuntu 4.8.4-2ubuntu1~14.04.3) 4.8.4
 // error message: ‘A’ is not an accessible base of ‘B’
 // struct B : protected A {
-struct B : private A {
+// struct B : private A {
+  struct B {
   void push() { LOG_MACROS(B, push); };
 };
 
 struct C {
-  void push() { LOG_MACROS(C, push); };
+  void push() { LOG_MACROS(C, add); };
 };
 
 #endif  // __TYPES_EXAMPLE_HPP__
